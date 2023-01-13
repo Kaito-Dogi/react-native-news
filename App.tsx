6dfe8418b478e4421e2da7bba69e619689ed9507
Kaito-Dogi <kendobu0405@gmail.com>
2023-01-14 02:58:36 +0900

    feat: SafeAreaView の中に FlatList を表示

--- App.tsx
+++ App.tsx
@@ -1,26 +1,29 @@
-import { StyleSheet, View } from 'react-native';
+import { FlatList, ListRenderItemInfo, SafeAreaView, StyleSheet } from 'react-native';
 import { ListItem } from './src/components/ListItem';
-
-const dummyImageUri: string = 'https://placehold.jp/100x100.png';
-const dummyTitle: string =
-  'A React component for displaying text. Text supports nesting, styling, and touch handling. In the following example, the nested title and body text will inherit the fontFamily from styles.baseText, but the title provides its own additional styles. The title and body will stack on top of each other on account of the literal newlines:';
-const dummyAuthor: string = 'React Native';
+import { articles } from './src/const/articles';
+import { Article } from './src/types/Article';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <ListItem imageUri={dummyImageUri} title={dummyTitle} author={dummyAuthor} />
-      <ListItem imageUri={dummyImageUri} title={dummyTitle} author={dummyAuthor} />
-      <ListItem imageUri={dummyImageUri} title={dummyTitle} author={dummyAuthor} />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={(article: ListRenderItemInfo<Article>) => (
+          <ListItem
+            imageUri={article.item.urlToImage}
+            title={article.item.title}
+            author={article.item.author}
+          />
+        )}
+        keyExtractor={(article) => article.title}
+      />
+    </SafeAreaView>
   );
 }
