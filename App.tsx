1fbd01bce96a4158fd45dc3578a5dfb96132733f
Kaito-Dogi <kendobu0405@gmail.com>
2023-01-14 02:15:00 +0900

    feat: ニュースの内容部分を実装

--- App.tsx
+++ App.tsx
@@ -1,19 +1,27 @@
-import { Image, StyleSheet, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.itemContainer}>
         <View style={styles.imageContainer}>
-          <Image style={styles.image} source={{ uri: imageUri }} />
+          <Image style={styles.image} source={{ uri: dummyImageUri }} />
         </View>
-        <View style={styles.contentContainer}></View>
+        <View style={styles.contentContainer}>
+          <Text numberOfLines={3} style={styles.sentence}>
+            {dummySentence}
+          </Text>
+          <Text style={styles.providerName}>{dummyProviderName}</Text>
+        </View>
       </View>
     </View>
   );
 }
 
-const imageUri: string = 'https://placehold.jp/100x100.png';
+const dummyImageUri: string = 'https://placehold.jp/100x100.png';
+const dummySentence: string =
+  'A React component for displaying text. Text supports nesting, styling, and touch handling. In the following example, the nested title and body text will inherit the fontFamily from styles.baseText, but the title provides its own additional styles. The title and body will stack on top of each other on account of the literal newlines:';
+const dummyProviderName: string = 'React Native';
 
 const styles = StyleSheet.create({
   container: {
@@ -32,15 +40,22 @@ const styles = StyleSheet.create({
   imageContainer: {
     height: 100,
     width: 100,
-    backgroundColor: 'red',
   },
   contentContainer: {
     height: 100,
-    backgroundColor: 'blue',
     flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    padding: 10,
   },
   image: {
     height: 100,
     width: 100,
+  },
+  sentence: {
+    fontSize: 16,
+  },
+  providerName: {
+    fontSize: 12,
   },
 });
