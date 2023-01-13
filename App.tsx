95bf2547754447d8800f715689cd03730e3ea846
Kaito-Dogi <kendobu0405@gmail.com>
2023-01-14 02:07:25 +0900

    feat: 画像部分を実装

--- App.tsx
+++ App.tsx
@@ -1,15 +1,19 @@
-import { StyleSheet, View } from 'react-native';
+import { Image, StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.itemContainer}>
-        <View style={styles.imageContainer}></View>
+        <View style={styles.imageContainer}>
+          <Image style={styles.image} source={{ uri: imageUri }} />
+        </View>
         <View style={styles.contentContainer}></View>
       </View>
     </View>
   );
 }
+
+const imageUri: string = 'https://placehold.jp/100x100.png';
 
 const styles = StyleSheet.create({
   container: {
@@ -34,5 +38,9 @@ const styles = StyleSheet.create({
     height: 100,
     backgroundColor: 'blue',
     flex: 1,
+  },
+  image: {
+    height: 100,
+    width: 100,
   },
 });
