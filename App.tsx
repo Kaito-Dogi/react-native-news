776df43831e07e1038a25076eff910b8a1d5cfee
Kaito-Dogi <kendobu0405@gmail.com>
2023-01-14 01:54:21 +0900

    feat: 長方形を作成

--- App.tsx
+++ App.tsx
@@ -1,11 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.itemContainer}></View>
     </View>
   );
 }
@@ -16,5 +14,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  itemContainer: {
+    height: 100,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
   },
 });
