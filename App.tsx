c413426e089d74e437927f28662ec77b99f75c78
Kaito-Dogi <kendobu0405@gmail.com>
2023-01-18 02:27:50 +0900

    feat: App.tsx に Navigation を追加

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { HomeScreen } from './src/screens/HomeScreen';
+
+const Stack = createStackNavigator();
+const App: React.FC = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
