1f4d505767a5d189b16b306a2cf31bbd343e3963
Kaito-Dogi <kendobu0405@gmail.com>
2023-10-31 21:17:40 +0900

    feat: provide store in App

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { AppNavigator } from './src/navigation/AppNavigator';
+import { Provider } from 'react-redux';
+import { store } from 'src/store';
 
 const App: React.FC = () => {
-  return <AppNavigator />;
+  return <Provider store={store}>
+    <AppNavigator />
+  </Provider>;
 };
 
 export default App;
